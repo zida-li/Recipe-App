2d5bba429fba1f3bb358583dbe517d1d2b604352
Zida Li <zidali8@gmail.com>
2021-11-03 20:16:18 -0400

    Renaming
    -Found a few more missed names that slipped through.

--- app/src/androidTest/java/us/zidali/recipeapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/us/zidali/recipeapp/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.recipeappfivelearning", appContext.packageName)
+        assertEquals("us.zidali.recipeapp", appContext.packageName)
     }
 }
